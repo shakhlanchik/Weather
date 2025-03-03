a031d8b67f3c6ca4806dbb7734031ef58274f9ed
shakhlanchik <shakhlanchik@gmail.com>
2025-03-03 10:27:04 +0300

    :)

---
+++ src/main/java/com/demo/weatherapi/controller/WeatherController.java
@@ -0,0 +1,13 @@
+package com.demo.weatherapi.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherController.class, args);
+    }
+
+}
