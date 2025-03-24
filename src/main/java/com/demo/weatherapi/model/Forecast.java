736143bf3f9f76d3c8f30833640f6532a17f52c9
shakhlanchik <shakhlanchik@gmail.com>
2025-03-24 11:02:00 +0300

    init commit

--- src/main/java/com/demo/weatherapi/model/Forecast.java
+++ src/main/java/com/demo/weatherapi/model/Forecast.java
@@ -9,12 +9,12 @@ import lombok.Setter;
 
 public class Forecast {
     private int cityId;
-    private final List<DailyWeather> forecast;
+    private final List<DailyWeather> tempForecast;
     private List<DailyWeather> dailyForecast;
 
     public Forecast(int cityId, List<DailyWeather> tempForecast) {
         this.cityId = cityId;
-        this.forecast = tempForecast;
+        this.tempForecast = tempForecast;
     }
 
 }
