7a9c50d0665a44d1a2576941ff43aa417eff18ac
shakhlanchik <shakhlanchik@gmail.com>
2025-04-24 23:40:22 +0300

    lab 2

--- src/main/java/com/demo/weatherapi/repository/WeatherRepository.java
+++ src/main/java/com/demo/weatherapi/repository/WeatherRepository.java
@@ -1,10 +1,8 @@
 package com.demo.weatherapi.repository;
 
 import com.demo.weatherapi.model.Weather;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
-    List<Weather> findByConditionContainingIgnoreCase(String condition);
     //магия
 }
