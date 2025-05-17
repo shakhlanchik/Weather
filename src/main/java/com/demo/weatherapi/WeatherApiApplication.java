d601b2f5c068f2af21e0b13e31a58a051d45d36e
shakhlanchik <shakhlanchik@gmail.com>
2025-05-17 14:41:22 +0300

    lab 4

--- src/main/java/com/demo/weatherapi/WeatherApiApplication.java
+++ src/main/java/com/demo/weatherapi/WeatherApiApplication.java
@@ -1,25 +1,13 @@
 package com.demo.weatherapi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherApiApplication {
-    private static final Logger logger = LoggerFactory.getLogger(WeatherApiApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(WeatherApiApplication.class, args);
-        logger.info("=== Проверка логов ===");  // Сообщение появится в weatherapi.log
     }
-}
 
-//@SpringBootApplication
-//public class WeatherApiApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(WeatherApiApplication.class, args);
-//    }
-//
-//}
+}
